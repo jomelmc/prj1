dbec5eab87938b2e03682049fb0531d271867c83
jomelmc <jomelmcdonald@gmail.com>
2018-11-20 07:23:29 -0500

    prj1

---
+++ prj1/Form1.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+    	Me.btn_Inicio = New System.Windows.Forms.Button()
+    	Me.txt_Indice = New System.Windows.Forms.TextBox()
+    	Me.label4 = New System.Windows.Forms.Label()
+    	Me.label3 = New System.Windows.Forms.Label()
+    	Me.label2 = New System.Windows.Forms.Label()
+    	Me.label1 = New System.Windows.Forms.Label()
+    	Me.label5 = New System.Windows.Forms.Label()
+    	Me.SuspendLayout
+    	'
+    	'btn_Inicio
+    	'
+    	Me.btn_Inicio.Font = New System.Drawing.Font("Montserrat", 36!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+    	Me.btn_Inicio.Location = New System.Drawing.Point(287, 125)
+    	Me.btn_Inicio.Name = "btn_Inicio"
+    	Me.btn_Inicio.Size = New System.Drawing.Size(171, 66)
+    	Me.btn_Inicio.TabIndex = 0
+    	Me.btn_Inicio.Text = "Inicio"
+    	Me.btn_Inicio.UseVisualStyleBackColor = true
+    	'
+    	'txt_Indice
+    	'
+    	Me.txt_Indice.Location = New System.Drawing.Point(496, 125)
+    	Me.txt_Indice.Name = "txt_Indice"
+    	Me.txt_Indice.ReadOnly = true
+    	Me.txt_Indice.Size = New System.Drawing.Size(40, 20)
+    	Me.txt_Indice.TabIndex = 1
+    	Me.txt_Indice.Visible = false
+    	'
+    	'label4
+    	'
+    	Me.label4.Font = New System.Drawing.Font("Montserrat", 8.999999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+    	Me.label4.Location = New System.Drawing.Point(160, 69)
+    	Me.label4.Name = "label4"
+    	Me.label4.Size = New System.Drawing.Size(396, 23)
+    	Me.label4.TabIndex = 11
+    	Me.label4.Text = "Nombre: Jomel Mc Donald   Cédula: 3-740-1458   Grupo: 1LS-232"
+    	'
+    	'label3
+    	'
+    	Me.label3.Font = New System.Drawing.Font("Montserrat", 8.999999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+    	Me.label3.Location = New System.Drawing.Point(242, 46)
+    	Me.label3.Name = "label3"
+    	Me.label3.Size = New System.Drawing.Size(237, 23)
+    	Me.label3.TabIndex = 10
+    	Me.label3.Text = "Licenciatura en Desarollo de Software"
+    	'
+    	'label2
+    	'
+    	Me.label2.Font = New System.Drawing.Font("Montserrat", 8.999999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+    	Me.label2.Location = New System.Drawing.Point(218, 31)
+    	Me.label2.Name = "label2"
+    	Me.label2.Size = New System.Drawing.Size(298, 23)
+    	Me.label2.TabIndex = 9
+    	Me.label2.Text = "FACULTAD DE INGENIERÍA DE SISTEMAS COMPUTACIONALES"
+    	'
+    	'label1
+    	'
+    	Me.label1.Font = New System.Drawing.Font("Montserrat", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+    	Me.label1.Location = New System.Drawing.Point(172, 13)
+    	Me.label1.Name = "label1"
+    	Me.label1.Size = New System.Drawing.Size(383, 23)
+    	Me.label1.TabIndex = 8
+    	Me.label1.Text = "UNIVERSIDAD TECNOLÓGICA DE PANAMÁ"
+    	'
+    	'label5
+    	'
+    	Me.label5.Font = New System.Drawing.Font("Montserrat", 18!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+    	Me.label5.Location = New System.Drawing.Point(464, 122)
+    	Me.label5.Name = "label5"
+    	Me.label5.Size = New System.Drawing.Size(30, 30)
+    	Me.label5.TabIndex = 12
+    	Me.label5.Text = "="
+    	Me.label5.Visible = false
+    	'
+    	'Form1
+    	'
+    	Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+    	Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    	Me.ClientSize = New System.Drawing.Size(753, 262)
+    	Me.Controls.Add(Me.label5)
+    	Me.Controls.Add(Me.label4)
+    	Me.Controls.Add(Me.label3)
+    	Me.Controls.Add(Me.label2)
+    	Me.Controls.Add(Me.label1)
+    	Me.Controls.Add(Me.txt_Indice)
+    	Me.Controls.Add(Me.btn_Inicio)
+    	Me.Name = "Form1"
+    	Me.Text = "Form1"
+    	Me.ResumeLayout(false)
+    	Me.PerformLayout
+    End Sub
+    Private label5 As System.Windows.Forms.Label
+    Private label1 As System.Windows.Forms.Label
+    Private label2 As System.Windows.Forms.Label
+    Private label3 As System.Windows.Forms.Label
+    Private label4 As System.Windows.Forms.Label
+
+    Friend WithEvents btn_Inicio As Button
+    Friend WithEvents txt_Indice As TextBox
+End Class
---
+++ prj1/Form1.vb
@@ -0,0 +1,176 @@
+﻿Public Class Form1
+	Dim vtxt_Operacion(6) As TextBox
+	Dim vbtn_Random (3) As Button
+	Dim vbtn_Operador(3) As Button
+	Dim txt_Operacion As TextBox
+    Dim btn_Operador As Button
+    Dim btn_Random As Button
+    Dim current_Operator
+    Dim counter As Integer
+    Dim i As Integer
+    Dim numeros_presionados As Array
+    Private Sub btn_Inicio_Click(sender As Object, e As EventArgs) Handles btn_Inicio.Click
+        btn_Inicio.Visible="false"
+        txt_Indice.Text = "0"
+        txt_Indice.Visible = "True"
+        label5.Visible = "True"
+        'PANEL PARA INGRESAR OPERACION
+        For i = 0 To 6
+            txt_Operacion = New TextBox()
+            txt_Operacion.Size = New Size(40, 20)
+            txt_Operacion.Location = New Point(150 + i * (txt_Operacion.Width + 5), 125)
+            txt_Operacion.ReadOnly = true
+            Controls.Add(txt_Operacion)
+            vtxt_Operacion(i) = txt_Operacion
+        Next
+        'NUMEROS ALEATORIOS
+        For i=0 To 3
+        	Dim rnd As System.Random = New System.Random()
+        	btn_Random = New Button()
+            btn_Random.Size = New Size(40, 20)
+            btn_Random.Location = New Point(200 + i * (btn_Random.Width + 5), 200)
+            btn_Random.Text = rnd.Next(1, 10)
+            Controls.Add(btn_Random)
+            vbtn_Random(i) = btn_Random
+            AddHandler btn_Random.Click, AddressOf btn_Random_Click
+            
+            'OPERADORES
+            btn_Operador = New Button()
+            btn_Operador.Size = New Size(40, 20)
+            
+            Select Case i
+            	Case 0
+            		btn_Operador.Text = "+"
+            	Case 1
+            		btn_Operador.Text = "-"
+            	Case 2
+            		btn_Operador.Text = "*"
+            	Case 3
+            		btn_Operador.Text = "/"
+           	End Select
+           	
+           	btn_Operador.Location = New Point(200 + i * (btn_Operador.Width + 5), 225)
+            Controls.Add(btn_Operador)
+            vbtn_Operador(i) = btn_Operador
+            AddHandler btn_Operador.Click, AddressOf btn_Operador_Click
+            
+        Next
+        
+    End Sub
+    
+    'CLICK A OPERADOR
+    Private Sub btn_Operador_Click(sender As Object, e As EventArgs)
+    	
+    	'AUMENTA EL MOSTRADOR
+   	 	If (counter > 1 And counter <= 6) Then
+    			counter = counter + 1
+   	 	End If
+    	
+    	Dim ind As Integer
+        Dim btn_Temp_op As Button
+        btn_Temp_op = CType(sender, Button)
+        
+        current_Operator = btn_Temp_op.Text
+        
+        'MUESTRA LOS OPERADORES EN LA BARRA DE OPERACION
+        vtxt_Operacion(counter).Text = btn_Temp_op.Text
+        
+        'DESHABILITA LOS OPERADORES
+        For ind=0 To 3
+        	vbtn_Operador(ind).Enabled="false"
+        Next
+        
+        
+        'DESHABILITA LOS NUMERO YA UTILIZADOS
+        numeros_presionados(counter-1) = Val(vtxt_Operacion(counter-1).Text)
+   
+        For ind=0 To numeros_presionados.Length
+        	
+        	For i=0 To vbtn_Random.Length
+        		If (vbtn_Random(i).Text = numeros_presionados(ind).Text) Then
+        			vbtn_Random(i).Enabled = "true"
+        		End If
+        	Next
+        	
+        Next
+        
+        
+        'HABILITA LOS RANDOM
+        For ind=0 To 3
+        	vbtn_Random(ind).Enabled="true"
+        Next
+        
+	End Sub
+	
+	
+	'CLICK A RANDOM    
+	Private Sub btn_Random_Click(sender As Object, e As EventArgs)
+		
+		Dim ind As Integer
+        Dim btn_Temp As Button
+        btn_Temp = CType(sender, Button)
+        
+        'AUMENTA EL MOSTRADOR 
+        If (counter > 0 And counter <= 6) Then
+			counter = counter + 1	
+        End If
+        
+        'MUESTRA LOS OPERADORES EN LA BARRA DE OPERACION
+        vtxt_Operacion(counter).Text = Val(btn_Temp.Text)
+		
+		'DESHABILITA LOS OPERADORES        
+        For ind=0 To 3
+        	vbtn_Random(ind).Enabled="false"
+        Next
+        
+        'HABILITA LOS NUMEROS
+        For ind=0 To 3
+        	vbtn_Operador(ind).Enabled="true"
+        Next
+        
+        'REALIZA OPERACION
+        If (counter = 0) Then
+			
+			'MUESTRA EL PRIMER VALOR EN LA CELDA DE RESULTADO
+			txt_Indice.Text = Val(vtxt_Operacion(counter).Text)
+		
+		Elseif (counter > 2) Then
+			
+			'REALIZA LA OPERACION DESPUES DE LA PRIMERA 
+        	Select Case current_Operator
+        		Case "+"
+        			txt_Indice.Text = Val(txt_Indice.Text) + Val (vtxt_Operacion(counter).Text)
+        		Case "-"
+        			txt_Indice.Text = Val(txt_Indice.Text) - Val (vtxt_Operacion(counter).Text)
+        		Case "*"
+        			txt_Indice.Text = Val(txt_Indice.Text) * Val (vtxt_Operacion(counter).Text)
+        		Case "/"
+        			txt_Indice.Text = Val(txt_Indice.Text) / Val (vtxt_Operacion(counter).Text)
+        	End Select
+        	
+		ElseIf (counter = 2) Then
+			
+			'REALIZA LA PRIMERA OPERACION			
+        	 Select Case current_Operator
+        		Case "+"
+        			txt_Indice.Text = Val(vtxt_Operacion(counter-2).Text) + Val (vtxt_Operacion(counter).Text)
+        		Case "-"
+        			txt_Indice.Text = Val(vtxt_Operacion(counter-2).Text) - Val (vtxt_Operacion(counter).Text)
+        		Case "*"
+        			txt_Indice.Text = Val(vtxt_Operacion(counter-2).Text) * Val (vtxt_Operacion(counter).Text)
+        		Case "/"
+        			txt_Indice.Text = Val(vtxt_Operacion(counter-2).Text) / Val (vtxt_Operacion(counter).Text)
+        	 End Select
+
+        	 
+		End If
+		
+        
+        'AUMENTA EL CONTADOR DESPUÉS DEL PRIMER CASO
+        If (counter = 0) Then
+			counter = 1
+        End If
+        
+    End Sub
+    
+End Class
